9318f012bc6cc35ecd7804489fe0c8ddc2c8dd31
Devin Dow <DevinDow@gmail.com>
2018-01-29 13:04:40 -0800

    Drawing text to imgPose.

--- app/src/main/java/com/devindow/myfitnessroutines/PlayRoutineActivity.java
+++ app/src/main/java/com/devindow/myfitnessroutines/PlayRoutineActivity.java
@@ -1,8 +1,10 @@
 package com.devindow.myfitnessroutines;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PlayRoutineActivity extends AppCompatActivity {
@@ -18,5 +20,12 @@ public class PlayRoutineActivity extends AppCompatActivity {
 
 		TextView txtRoutineName = (TextView) findViewById(R.id.txtRoutineName);
 		txtRoutineName.setText(routine.Name);
+
+		ImageView imgPose = (ImageView) findViewById(R.id.imgPose);
+		Step step1 = routine.Steps.get(0);
+		int w = imgPose.getWidth();
+		int h = imgPose.getHeight();
+		Bitmap bitmap = step1.Pose.getBitmap(40, 40);
+		imgPose.setImageBitmap(bitmap);
 	}
 }
--- app/src/main/java/com/devindow/myfitnessroutines/Pose.java
+++ app/src/main/java/com/devindow/myfitnessroutines/Pose.java
@@ -1,5 +1,10 @@
 package com.devindow.myfitnessroutines;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.media.Image;
 
 import java.io.Serializable;
@@ -10,10 +15,22 @@ import java.io.Serializable;
 
 public class Pose implements Serializable {
 
+    // Public Fields
     public String Name;
     public Category Category;
     public boolean TwoSides;
-    public Image Image;
+
+
+    // Public Properties
+    public Bitmap getBitmap(int w, int h) {
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint p = new Paint();
+        p.setColor(Color.BLUE);
+        p.setTextSize(8);
+        canvas.drawText("Hello", w/2, h/2, p);
+        return bitmap;
+    }
 
 
     // Constructors
