1a1dfd31318c528d7b41c32238c53e5d73e09462
Devin Dow <DevinDow@gmail.com>
2018-01-29 22:32:00 -0800

    WIP: Drawing character at inch scale.

---
+++ app/src/main/java/com/devindow/myfitnessroutines/FrontalPose.java
@@ -0,0 +1,68 @@
+package com.devindow.myfitnessroutines;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.media.Image;
+
+import java.io.Serializable;
+
+/**
+ * Created by Devin on 1/27/2018.
+ */
+
+public class FrontalPose extends Pose {
+
+    // Public Properties
+    public Bitmap getBitmap(int w, int h) {
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.translate(w/2, h-1); // Origin a floor center
+        canvas.scale(10, -10); // up is positive Y, 10x scale
+
+        Paint p = new Paint();
+        p.setStrokeCap(Paint.Cap.ROUND);
+        p.setStrokeJoin(Paint.Join.ROUND);
+        p.setColor(Color.BLACK);
+
+        // Draw Floor
+        p.setStrokeWidth(1);
+        canvas.drawLine(-w/2, 0,w/2,0, p);
+
+        // Draw Head
+        p.setStrokeWidth(headSize);
+        canvas.drawPoint(headX, headY, p);
+
+        // Draw Torso
+        p.setStrokeWidth(torsoThickness);
+        canvas.drawLine(getNeckX(), getNeckY(), waistX, waistY, p);
+
+        // Draw Arms
+        p.setStrokeWidth(armThickness);
+        int rShoulderX = getNeckX() + torsoThickness/2;
+        canvas.drawLine(rShoulderX, getNeckY(), rHandX, rHandY, p);
+        int lShoulderX = getNeckX() - torsoThickness/2;
+        canvas.drawLine(lShoulderX, getNeckY(), lHandX, lHandY, p);
+
+        // Draw Legs
+        p.setStrokeWidth(legThickness);
+        canvas.drawLine(waistX + legThickness/2, waistY, rFootX, rFootY, p);
+        canvas.drawLine(waistX - legThickness/2, waistY, lFootX, lFootY, p);
+
+        return bitmap;
+    }
+
+
+    // Constructors
+    public FrontalPose(String name, Category category) { super(name, category); }
+    public FrontalPose(String name, Category category, boolean twoSides) { super(name, category, twoSides); }
+
+
+    // Overrides
+    @Override
+    public String toString() {
+        return this.Name;
+    }
+}
--- app/src/main/java/com/devindow/myfitnessroutines/Pose.java
+++ app/src/main/java/com/devindow/myfitnessroutines/Pose.java
@@ -13,103 +13,40 @@ import java.io.Serializable;
  * Created by Devin on 1/27/2018.
  */
 
-public class Pose implements Serializable {
+public abstract class Pose implements Serializable {
 
     // Public Fields
     public String Name;
     public Category Category;
     public boolean TwoSides;
 
-    public boolean IsProfile = false;
+    public int headX = 0;
+    public int headY = 60;
 
+    public int waistX = 0;
+    public int waistY = 34;
 
-    // Private Fields
-    private int headX = 0;
-    private int headY = 60;
+    public int rHandX = 25;
+    public int rHandY = 60;
+    public int lHandX = -25;
+    public int lHandY = 60;
 
-    private int waistX = 0;
-    private int waistY = 34;
-
-    private int rHandX = 25;
-    private int rHandY = 60;
-    private int lHandX = -25;
-    private int lHandY = 60;
-
-    private int rFootX = 4;
-    private int rFootY = 0;
-    private int lFootX = -4;
-    private int lFootY = 0;
-
-
-    // Public Properties
-    public void setHeadX(int headX) { this.headX = headX; }
-    public void setHeadY(int headY) { this.headY = headY; }
+    public int rFootX = 4;
+    public int rFootY = 0;
+    public int lFootX = -4;
+    public int lFootY = 0;
 
 
     // Private Properties
-    private int getNeckX() { return headX; } // TODO
-    private int getNeckY() { return headY-8; } // TODO
+    protected int getNeckX() { return waistX + (int)Math.round(0.7*(headX-waistX)); } // TODO
+    protected int getNeckY() { return waistY + (int)Math.round(0.7*(headY-waistY)); } // TODO
 
 
-    // Private Constants
-    private static final int headSize = 10;
-    private static final int torsoWidth = 16;
-    private static final int torsoThickness = 10;
-    private static final int armThickness = 4;
-    private static final int legThickness = 6;
-
-
-    // Public Properties
-    public Bitmap getBitmap(int w, int h) {
-        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        canvas.translate(w/2, h-1); // Origin a floor center
-        canvas.scale(10, -10); // up is positive Y, 10x scale
-
-        Paint p = new Paint();
-        p.setStrokeCap(Paint.Cap.ROUND);
-        p.setStrokeJoin(Paint.Join.ROUND);
-        p.setColor(Color.BLACK);
-
-        // Draw Floor
-        p.setStrokeWidth(1);
-        canvas.drawLine(-w/2, 0,w/2,0, p);
-
-        // Draw Head
-        p.setStrokeWidth(headSize);
-        canvas.drawPoint(headX, headY, p);
-
-        // Draw Torso
-        if (IsProfile)
-            p.setStrokeWidth(torsoWidth);
-        else
-            p.setStrokeWidth(torsoThickness);
-        canvas.drawLine(getNeckX(), getNeckY(), waistX, waistY, p);
-
-        // Draw Arms
-        p.setStrokeWidth(armThickness);
-        if (IsProfile) {
-
-        }
-        else {
-            int rShoulderX = getNeckX() + torsoThickness/2;
-            canvas.drawLine(rShoulderX, getNeckY(), rHandX, rHandY, p);
-            int lShoulderX = getNeckX() - torsoThickness/2;
-            canvas.drawLine(lShoulderX, getNeckY(), lHandX, lHandY, p);
-        }
-
-        // Draw Legs
-        p.setStrokeWidth(legThickness);
-        if (IsProfile) {
-
-        }
-        else {
-            canvas.drawLine(waistX + legThickness/2, waistY, rFootX, rFootY, p);
-            canvas.drawLine(waistX - legThickness/2, waistY, lFootX, lFootY, p);
-        }
-
-        return bitmap;
-    }
+    // Protected Constants
+    protected static final int headSize = 10;
+    protected static final int torsoThickness = 10;
+    protected static final int armThickness = 4;
+    protected static final int legThickness = 6;
 
 
     // Constructors
@@ -124,6 +61,10 @@ public class Pose implements Serializable {
         this.Category = category;
         this.TwoSides = twoSides;
     }
+
+
+    // Public Abstract Methods
+    public abstract Bitmap getBitmap(int w, int h);
 
 
     // Overrides
