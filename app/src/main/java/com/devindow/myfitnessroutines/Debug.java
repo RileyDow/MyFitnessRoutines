4ebe1afc61a5f4cae7a305507c213198f90b132b
Devin Dow <DevinDow@gmail.com>
2018-02-12 10:43:11 -0800

    Added conditional comiplation for Debug.

---
+++ app/src/main/java/com/devindow/myfitnessroutines/Debug.java
@@ -0,0 +1,10 @@
+package com.devindow.myfitnessroutines;
+
+/**
+ * Created by Devin on 2/12/2018.
+ */
+
+public final class Debug {
+	// Set to false to allow compiler to identify and eliminate unreachable code.
+	public static final boolean ON = true;
+}
