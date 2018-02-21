5c58cb24603804542ffe8012222ec61376841086
Devin Dow <DevinDow@gmail.com>
2018-02-21 12:22:38 -0800

    APK

--- app/src/main/java/com/devindow/myfitnessroutines/Debug.java
+++ app/src/main/java/com/devindow/myfitnessroutines/Debug.java
@@ -12,7 +12,7 @@ import java.util.Random;
 public final class Debug {
 
 	// Public Static Constants
-	public static final boolean on = true; // Set to false to allow compiler to identify and eliminate unreachable code.
+	public static final boolean on = false; // Set to false to allow compiler to identify and eliminate unreachable code.
 
 	public static final boolean colors = false; // Set to true to help debug Pose bitmap issues using unique colors for body parts.
 
