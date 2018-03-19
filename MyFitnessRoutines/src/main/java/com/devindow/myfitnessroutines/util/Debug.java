996bbcc3c15072cc1d768225945c5e1ba1ba2cda
Devin Dow <DevinDow@gmail.com>
2018-03-19 09:01:35 -0700

    versionCode 9 = "0.9-beta"

--- MyFitnessRoutines/src/main/java/com/devindow/myfitnessroutines/util/Debug.java
+++ MyFitnessRoutines/src/main/java/com/devindow/myfitnessroutines/util/Debug.java
@@ -9,9 +9,9 @@ import android.util.Log;
 public final class Debug {
 
 	// Public Static Constants
-	static final int logLevel = Log.DEBUG; // ERROR = 6, WARN = 5, INFO = 4, DEBUG = 3, VERBOSE = 2
+	static final int logLevel = Log.INFO; // ERROR = 6, WARN = 5, INFO = 4, DEBUG = 3, VERBOSE = 2
 
-	public static final boolean on = true; // Set to false to allow compiler to identify and eliminate unreachable code.
+	public static final boolean on = false; // Set to false to allow compiler to identify and eliminate unreachable code.
 
 	public static final String TAG_ERROR = " **** ERROR **** ";
 	public static final String TAG_ENTER = " ** ";
